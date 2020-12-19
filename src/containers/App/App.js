8fedc0ac69b74ec435f7b850406dbcfedee5747a
Leo Dalbert <leo@dalbert.us>
2020-12-18 15:39:42 -1000

    init commit

---
+++ src/containers/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const App = (props) => {
+  return (
+    <div>
+      <div>test</div>
+    </div>
+  );
+};
+
+App.propTypes = {};
+
+export default App;
